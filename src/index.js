c2006fecc03e5fd203cdf03cfa54d4dbe222bb72
Maksim Aniska <aniskamaksim@gmail.com>
2022-07-27 00:44:01 +0300

    feat: add working code

--- src/index.js
+++ src/index.js
@@ -37,10 +37,35 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
-}
-
-module.exports = {
-    decode
-}
+function convertToDotDash(sOf2) {
+    if (sOf2 == "11") {
+        return "-";
+    } else if (sOf2 == "10") {
+        return ".";
+    } else {
+        return "";
+    }
+    }
+    function decode(expr) {
+       let array1 = expr.match(/.{1,10}/g);
+       let result = "";
+    for (stringOf10 of array1) {
+        console.log(stringOf10);
+        if (stringOf10 == "**********") {
+            result += " ";
+        } else {
+        stringOf2Array = stringOf10.match(/.{1,2}/g);
+        console.log(stringOf2Array);
+        let letter = "";
+        for (stringOf2 of stringOf2Array) {
+            letter += convertToDotDash(stringOf2);
+        }
+        console.log(letter);
+        result += MORSE_TABLE[letter];
+        }
+    }
+    return result;
+    }
+    module.exports = {
+        decode
+    }
